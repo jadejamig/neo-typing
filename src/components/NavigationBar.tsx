7a167b1c5afc26ce5cf1bc67410c32818e3d7468
Jade Jamig <jadejamigdev@gmail.com>
2024-06-10 15:32:13 +0800

    mobile nav

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import Button from './neo-brutalism/Button'
+import Dropdown from './neo-brutalism/Dropdown';
 
 export default function NavigationBar() {
   const navigate = useNavigate();
@@ -7,18 +8,36 @@ export default function NavigationBar() {
   const handleNaavigate = () => {
     navigate("/");
   }
+
+  const items = [
+    {
+      name: 'Home',
+      link: '/dashboard',
+    },
+    {
+      name: 'Login',
+      link: '/dashboard',
+    },
+    {
+      name: 'Signup',
+      link: '/dashboard',
+    },
+  ]
   
   return (
     <div className="fixed top-0 z-50 flex justify-center items-center w-full bg-white h-24 outline outline-black">
         <div className='flex justify-between items-center max-w-7xl w-full px-8'>
             <div className='flex gap-8'>
-              <h1 onClick={handleNaavigate} className="text-5xl font-bold font-work cursor-pointer">Typrr</h1>
+              <h1 onClick={handleNaavigate} className="text-4xl md:text-5xl font-bold font-work cursor-pointer">Typrr</h1>
               
             </div>
-            <div className="flex gap-4">
-                <Button className="text-base" onClick={() => navigate('/dashboard')}>Home</Button>
-                <Button className="text-base">Login</Button>
-                <Button className="text-base">Signup</Button>
+            <div className="hidden sm:flex gap-4">
+                <Button className="text-xs sm:text-base" onClick={() => navigate('/dashboard')}>Home</Button>
+                <Button className="text-xs sm:text-base">Login</Button>
+                <Button className="text-xs sm:text-base">Signup</Button>
+            </div>
+            <div className='flex sm:hidden'>
+              <Dropdown items={items} text="Menu" />
             </div>
         </div>
     </div>
--- src/pages/app/Homepage.tsx
+++ src/pages/app/Homepage.tsx
@@ -13,11 +13,11 @@ export default function Homepage() {
     <div className="relative flex flex-col justify-center items-center h-dvh text-center w-screen bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="h-24"></div>
       <div className="flex flex-col justify-center items-center w-full gap-y-10">
-        <div className="text-7xl font-bold font-monsterrat">
+        <div className="text-4xl md:text-7xl font-bold font-monsterrat px-4">
           <span>Welcome to</span>
           <span className="text-mainAccent">{` Typrr`}</span>
         </div>
-        <p className="text-2xl"> Practice typing with a friend or anyone from anywhere in the world. </p>
+        <p className="text-lg md:text-2xl px-4"> Practice typing with a friend or anyone from anywhere in the world. </p>
         <Button onClick={navigateToPlay} className='text-lg w-32 justify-center font-monsterrat font-bold bg-main'>
           Play
         </Button>
